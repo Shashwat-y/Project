5210890737fb904460f029ed92c8f9ade776c030
Shashwat <shashwatmaths@gmail.com>
2023-08-19 18:40:52 +0530

    Started

---
+++ index.js
@@ -0,0 +1,12 @@
+const Input = document.getElementById('taskInput');
+const Button = document.getElementById('addButton');
+const List = document.getElementById('taskList');
+
+Button.addEventListener("click",Add);
+function Add(){
+    const taskText = Input.value.trim();
+    if(taskText != 0){
+
+    }
+
+}
