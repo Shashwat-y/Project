b4bf2be7956412e43cf931907fb1df5fb563e391
Shashwat <shashwatmaths@gmail.com>
2023-08-19 18:59:49 +0530

    Second-commit

--- index.js
+++ index.js
@@ -6,7 +6,25 @@ Button.addEventListener("click",Add);
 function Add(){
     const taskText = Input.value.trim();
     if(taskText != 0){
-
+        const Item = document.createElement('li');
+        Item.innerHTML = `
+            <span>${taskText}</span>
+            <button class="completeButton">Complete</button>
+            <button class="deleteButton">Delete</button>
+        `;
+        List.appendChild(Item);
+        Input.value = '';
+        Item.querySelector('.completeButton').addEventListener('click', completeTask);
+        Item.querySelector('.deleteButton').addEventListener('click', deleteTask);
     }
-
 }
+    function completeTask(event) {
+        const taskItem = event.target.parentNode;
+        taskItem.classList.toggle('completed');
+    }
+    
+    function deleteTask(event) {
+        const taskItem = event.target.parentNode;
+        List.removeChild(taskItem);
+    }
+    
